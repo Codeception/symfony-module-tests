e7f7b27337c5ced885ac1693977bb98a7a9cf92f
Tavo Nieves J <ganieves@outlook.com>
2020-12-14 00:58:50 -0500

    Remove yarn dependency (#2)
    Co-authored-by: ThomasLandauer <thomas@landauer.at>

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];

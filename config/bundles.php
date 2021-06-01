563af1c1790ad37473d7fcb67bd47eb23114c12d
Tavo Nieves J <ganieves@outlook.com>
2021-05-31 20:21:56 -0500

    Remove Foundry dependency

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];

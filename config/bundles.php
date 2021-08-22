8eb546cfc29f307b57cb23cbb57bb38e9bc2a131
Tavo Nieves J <64917965+TavoNiievez@users.noreply.github.com>
2021-08-21 22:29:36 -0500

    Use composer install, update dependencies (#18)

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
 ];

bd1fdfd4c26cbee5f8944e750cfc408eed3f22d8
TavoNiievez <ganieves@outlook.com>
2021-11-21 15:18:59 -0500

    Update code standards

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private ?int $id;
+    private ?int $id = null;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
--- tests/Functional/IssuesCest.php
+++ tests/Functional/IssuesCest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
-use App\Tests\FunctionalTester;
 use App\Entity\User;
+use App\Tests\FunctionalTester;
 use Doctrine\DBAL\Connection;
 
 final class IssuesCest

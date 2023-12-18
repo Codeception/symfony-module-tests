a0e7bf65b5744aed7fe761483a0c8d5670675c5b
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2023-12-18 21:38:55 +0200

    Support new event assertions (#25)
    * Update seeOrphanEvent test, add tests for seeEvent/dontSeeEvent
    * Add tests for seeEventTriggered/dontSeeEventTriggered to check event filtering
    * mark as skipped method tests not yet added
    * Remove sensio/framework-extra-bundle usage
    * Update codeception/module-symfony dependency

--- tests/Functional/EventsCest.php
+++ tests/Functional/EventsCest.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Event\UserRegisteredEvent;
 use App\Tests\FunctionalTester;
-use Sensio\Bundle\FrameworkExtraBundle\EventListener\SecurityListener;
 use Symfony\Bundle\FrameworkBundle\DataCollector\RouterDataCollector;
+use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\EventListener\ErrorListener;
+use Symfony\Component\HttpKernel\EventListener\LocaleListener;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 final class EventsCest
 {
+    /**
+     * @deprecated in favor of dontSeeEventListenerIsCalled
+     */
     public function dontSeeEventTriggered(FunctionalTester $I)
     {
         $I->amOnPage('/');
@@ -25,6 +32,9 @@ final class EventsCest
         $I->dontSeeEventListenerIsCalled(ErrorListener::class);
         $I->dontSeeEventListenerIsCalled(new ErrorListener());
         $I->dontSeeEventListenerIsCalled([ErrorListener::class, ErrorListener::class]);
+        // with events
+        $I->dontSeeEventListenerIsCalled(RouterListener::class, KernelEvents::EXCEPTION);
+        $I->dontSeeEventListenerIsCalled(RouterListener::class, [KernelEvents::RESPONSE, KernelEvents::EXCEPTION]);
     }
 
     public function dontSeeOrphanEvent(FunctionalTester $I)
@@ -35,34 +45,62 @@ final class EventsCest
             'password' => '123456',
             '_remember_me' => false
         ]);
-        $I->dontseeOrphanEvent();
+        $I->dontSeeOrphanEvent();
     }
 
+    public function dontSeeEvent(FunctionalTester $I)
+    {
+        $I->markTestSkipped();
+        $I->amOnPage('/');
+        $I->dontSeeEvent(KernelEvents::EXCEPTION);
+        $I->dontSeeEvent([new UserRegisteredEvent(), ConsoleEvents::COMMAND]);
+    }
+
+    /**
+     * @deprecated in favor of seeEventListenerIsCalled
+     */
     public function seeEventTriggered(FunctionalTester $I)
     {
         $I->amOnPage('/');
-        $I->seeEventTriggered(SecurityListener::class);
+        $I->seeEventTriggered(RouterListener::class);
         $I->seeEventTriggered(new RouterDataCollector());
-        $I->seeEventTriggered([SecurityListener::class, RouterDataCollector::class]);
+        $I->seeEventTriggered([RouterListener::class, RouterDataCollector::class]);
     }
 
     public function seeEventListenerIsCalled(FunctionalTester $I)
     {
         $I->amOnPage('/');
-        $I->seeEventListenerIsCalled(SecurityListener::class);
+        $I->seeEventListenerIsCalled(RouterListener::class);
         $I->seeEventListenerIsCalled(new RouterDataCollector());
-        $I->seeEventListenerIsCalled([SecurityListener::class, RouterDataCollector::class]);
+        $I->seeEventListenerIsCalled([RouterListener::class, RouterDataCollector::class]);
+        // with events
+        $I->seeEventListenerIsCalled(RouterListener::class, KernelEvents::REQUEST);
+        $I->seeEventListenerIsCalled(LocaleListener::class, [KernelEvents::REQUEST, KernelEvents::FINISH_REQUEST]);
     }
 
     public function seeOrphanEvent(FunctionalTester $I)
     {
-        $I->markTestIncomplete('To do: use a new event for this assertion');
         $I->amOnPage('/register');
+        $I->stopFollowingRedirects();
         $I->submitSymfonyForm('registration_form', [
             '[email]' => 'jane_doe@gmail.com',
             '[plainPassword]' => '123456',
             '[agreeTerms]' => true
         ]);
-        $I->seeOrphanEvent('security.authentication.success');
+        $I->seeOrphanEvent(UserRegisteredEvent::class);
+    }
+
+    public function seeEvent(FunctionalTester $I)
+    {
+        $I->markTestSkipped();
+        $I->amOnPage('/register');
+        $I->stopFollowingRedirects();
+        $I->submitSymfonyForm('registration_form', [
+            '[email]' => 'jane_doe@gmail.com',
+            '[plainPassword]' => '123456',
+            '[agreeTerms]' => true
+        ]);
+        $I->seeEvent(UserRegisteredEvent::class);
+        $I->seeEvent(KernelEvents::REQUEST, KernelEvents::FINISH_REQUEST);
     }
 }

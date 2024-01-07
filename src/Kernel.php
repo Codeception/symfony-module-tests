7e5f274e5454ce26092d9d0e75c694610b2257e5
TavoNiievez <ganieves@outlook.com>
2024-01-07 14:35:23 -0500

    Update to Symfony 6.4

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,45 +5,9 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function configureContainer(ContainerConfigurator $container): void
-    {
-        $configDir = $this->getConfigDir();
-
-        $container->import($configDir.'/{packages}/*.{php,yaml}');
-        $container->import($configDir.'/{packages}/'.$this->environment.'/*.{php,yaml}');
-
-        if (is_file($configDir.'/services.yaml')) {
-            $container->import($configDir.'/services.yaml');
-            $container->import($configDir.'/{services}_'.$this->environment.'.yaml');
-        } else {
-            $container->import($configDir.'/{services}.php');
-            $container->import($configDir.'/{services}_'.$this->environment.'.php');
-        }
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $configDir = $this->getConfigDir();
-
-        $routes->import($configDir.'/{routes}/'.$this->environment.'/*.{php,yaml}');
-        $routes->import($configDir.'/{routes}/*.{php,yaml}');
-
-        if (is_file($configDir.'/routes.yaml')) {
-            $routes->import($configDir.'/routes.yaml');
-        } else {
-            $routes->import($configDir.'/{routes}.php');
-        }
-
-        if (false !== ($fileName = (new \ReflectionObject($this))->getFileName())) {
-            $routes->import($fileName, 'annotation');
-        }
-    }
 }

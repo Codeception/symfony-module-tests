7cb367650c2c13bb567124793611d601612dfebc
Bernd Stellwag <burned@zerties.org>
2025-05-17 02:21:24 +0200

    Add command requiring user input and test passing inputs works (#28)

--- tests/Functional/ConsoleCest.php
+++ tests/Functional/ConsoleCest.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Command\AskForInputCommand;
 use App\Command\ExampleCommand;
 use App\Tests\Support\FunctionalTester;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\MissingInputException;
 
 final class ConsoleCest
 {
-    public function runSymfonyConsoleCommand(FunctionalTester $I)
+    public function runSymfonyConsoleCommand(FunctionalTester $I): void
     {
         // Call Symfony console without option
         $output = $I->runSymfonyConsoleCommand(ExampleCommand::getDefaultName());
@@ -28,5 +31,32 @@ final class ConsoleCest
             ['--something' => true]
         );
         $I->assertStringContainsString('Bye world!', $output);
+    }
+
+    public function runSymfonyConsoleCommandInput(FunctionalTester $I): void
+    {
+        // Confirmation question not confirmed
+        $output = $I->runSymfonyConsoleCommand(
+            AskForInputCommand::getDefaultName(),
+            consoleInputs: ['n'],
+            expectedExitCode: Command::FAILURE,
+        );
+        $I->assertStringContainsString('bye', $output);
+
+        // Exception on missing input
+        $I->expectThrowable(
+            MissingInputException::class,
+            fn () => $I->runSymfonyConsoleCommand(
+                AskForInputCommand::getDefaultName(),
+                consoleInputs: ['y'],
+            ),
+        );
+
+        // Multiple inputs
+        $output = $I->runSymfonyConsoleCommand(
+            AskForInputCommand::getDefaultName(),
+            consoleInputs: ['y', 'foobar'],
+        );
+        $I->assertStringContainsString("user input: 'foobar'", $output);
     }
 }

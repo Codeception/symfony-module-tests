6e7d7689c8853f103020a860e603180c8272a8ec
d-mitrofanov-v <dmitriy.mitrofanov.v@gmail.com>
2025-11-09 17:37:42 +0200

    fix alphabetical order of checks

--- tests/Functional/NotifierCest.php
+++ tests/Functional/NotifierCest.php
@@ -8,34 +8,6 @@ use App\Tests\Support\FunctionalTester;
 
 final class NotifierCest
 {
-    public function dontSeeNotificationIsSent(FunctionalTester $I)
-    {
-        $I->registerUser('john_doe@gmail.com', '123456', followRedirects: false);
-        // There is already an account with this notification
-        $I->dontSeeNotificationIsSent();
-    }
-
-    public function grabLastSentNotification(FunctionalTester $I)
-    {
-        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notification = $I->grabLastSentNotification();
-        $I->assertSame('Account created!', $notification->getSubject());
-    }
-
-    public function grabSentNotifications(FunctionalTester $I)
-    {
-        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notifications = $I->grabSentNotifications();
-        $subject = $notifications[0]->getSubject();
-        $I->assertSame('Account created!', $subject);
-    }
-
-    public function seeNotificationIsSent(FunctionalTester $I)
-    {
-        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $I->seeNotificationIsSent();
-    }
-
     public function assertNotificationSubjectContains(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
@@ -62,5 +34,33 @@ final class NotifierCest
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
         $notification = $I->getNotifierMessage();
         $I->assertNotificationTransportIsNotEqual($notification, 'chat');
+    }
+
+    public function dontSeeNotificationIsSent(FunctionalTester $I)
+    {
+        $I->registerUser('john_doe@gmail.com', '123456', followRedirects: false);
+        // There is already an account with this notification
+        $I->dontSeeNotificationIsSent();
+    }
+
+    public function grabLastSentNotification(FunctionalTester $I)
+    {
+        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
+        $notification = $I->grabLastSentNotification();
+        $I->assertSame('Account created!', $notification->getSubject());
+    }
+
+    public function grabSentNotifications(FunctionalTester $I)
+    {
+        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
+        $notifications = $I->grabSentNotifications();
+        $subject = $notifications[0]->getSubject();
+        $I->assertSame('Account created!', $subject);
+    }
+
+    public function seeNotificationIsSent(FunctionalTester $I)
+    {
+        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
+        $I->seeNotificationIsSent();
     }
 }

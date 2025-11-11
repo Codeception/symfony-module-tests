67ce7553073183e3916c1fefcd39bc1b6c32bc1c
dmitrii <88384601+d-mitrofanov-v@users.noreply.github.com>
2025-11-11 07:40:53 +0200

    Add notifier cest (#34)
    * add notifier cest
    * fix alphabetical order of checks

--- config/packages/framework.php
+++ config/packages/framework.php
@@ -30,6 +30,10 @@ return static function (FrameworkConfig $framework): void {
     $framework->mailer()
         ->dsn('%env(MAILER_DSN)%');
 
+    // Notifier
+    $framework->notifier()
+        ->chatterTransport('slack', '%env(NOTIFIER_DSN)%');
+
     // PropertyInfo
     $framework->propertyInfo()
         ->withConstructorExtractor(true);

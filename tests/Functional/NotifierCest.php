ad55d98363391158de151666315bb4bd29f65f76
d-mitrofanov-v <dmitriy.mitrofanov.v@gmail.com>
2025-11-28 07:03:27 +0200

    expect throwable for 5.4 in notifier assertions

--- tests/Functional/NotifierCest.php
+++ tests/Functional/NotifierCest.php
@@ -5,62 +5,79 @@ declare(strict_types=1);
 namespace App\Tests\Functional;
 
 use App\Tests\Support\FunctionalTester;
+use PHPUnit\Framework\Error\Warning;
 
 final class NotifierCest
 {
     public function assertNotificationSubjectContains(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notification = $I->getNotifierMessage();
-        $I->assertNotificationSubjectContains($notification, 'created!');
+        $I->expectThrowable(Warning::class, function () {
+            $notification = $I->getNotifierMessage();
+            $I->assertNotificationSubjectContains($notification, 'created!');
+        });
     }
 
     public function assertNotificationSubjectNotContains(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notification = $I->getNotifierMessage();
-        $I->assertNotificationSubjectNotContains($notification, 'Account not created!');
+        $I->expectThrowable(Warning::class, function () {
+            $notification = $I->getNotifierMessage();
+            $I->assertNotificationSubjectNotContains($notification, 'Account not created!');
+        });
     }
 
     public function assertNotificationTransportIsEqual(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notification = $I->getNotifierMessage();
-        $I->assertNotificationTransportIsEqual($notification);
+        $I->expectThrowable(Warning::class, function () {
+            $notification = $I->getNotifierMessage();
+            $I->assertNotificationTransportIsEqual($notification);
+        });
     }
 
     public function assertNotificationTransportIsNotEqual(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notification = $I->getNotifierMessage();
-        $I->assertNotificationTransportIsNotEqual($notification, 'chat');
+        $I->expectThrowable(Warning::class, function () {
+            $notification = $I->getNotifierMessage();
+            $I->assertNotificationTransportIsNotEqual($notification, 'chat');
+        });
     }
 
     public function dontSeeNotificationIsSent(FunctionalTester $I)
     {
         $I->registerUser('john_doe@gmail.com', '123456', followRedirects: false);
-        // There is already an account with this notification
-        $I->dontSeeNotificationIsSent();
+        $I->expectThrowable(Warning::class, function () {
+            // There is already an account with this notification
+            $I->dontSeeNotificationIsSent();
+        });
     }
 
     public function grabLastSentNotification(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notification = $I->grabLastSentNotification();
-        $I->assertSame('Account created!', $notification->getSubject());
+        $I->expectThrowable(Warning::class, function () {
+            $notification = $I->grabLastSentNotification();
+            $I->assertSame('Account created!', $notification->getSubject());
+        });
     }
 
     public function grabSentNotifications(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $notifications = $I->grabSentNotifications();
-        $subject = $notifications[0]->getSubject();
-        $I->assertSame('Account created!', $subject);
+        $I->expectThrowable(Warning::class, function () {
+            $notifications = $I->grabSentNotifications();
+            $subject = $notifications[0]->getSubject();
+            $I->assertSame('Account created!', $subject);
+        });
     }
 
     public function seeNotificationIsSent(FunctionalTester $I)
     {
         $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
-        $I->seeNotificationIsSent();
+        $I->expectThrowable(Warning::class, function () {
+            $I->seeNotificationIsSent();
+        });
     }
 }

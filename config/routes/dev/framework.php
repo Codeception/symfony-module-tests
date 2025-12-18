a6563eb0ad75e82ed2dbd5175dc31cf59c661328
W0rma <beck.worma@gmail.com>
2025-12-18 11:14:43 +0100

    Fix usage of deprecated .xml files

--- config/routes/dev/framework.php
+++ config/routes/dev/framework.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
-    $routes->import('@FrameworkBundle/Resources/config/routing/errors.xml')
+    $routes->import('@FrameworkBundle/Resources/config/routing/errors.php')
         ->prefix('/_error');
 };

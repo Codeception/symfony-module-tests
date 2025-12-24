fead06ca8072c5f3dad9e921c773e120e6184058
W0rma <beck.worma@gmail.com>
2025-12-24 11:29:26 +0100

    Update to Symfony 8

--- config/packages/prod/doctrine.php
+++ config/packages/prod/doctrine.php
@@ -7,8 +7,6 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 return App::config([
     'doctrine' => [
         'orm' => [
-            'auto_generate_proxy_classes' => false,
-            'proxy_dir' => '%kernel.build_dir%/doctrine/orm/Proxies',
             'entity_managers' => [
                 'default' => [
                     'query_cache_driver' => [

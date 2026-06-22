ecf17457c9caef172180b7ca82716c9d353eb2b4
Gustavo Nieves <ganieves@outlook.com>
2026-06-22 11:44:55 -0500

    Add regression tests for Doctrine listener security/request context (#34, #90, #150, #151)

---
+++ src/Doctrine/CurrentUserEventListener.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * Issue #34, event-listener path: a Doctrine event listener registered on the
+ * connection's event manager (not the ORM entity-listener resolver) must also
+ * see the logged-in user. This is the case the issue thread describes as a
+ * Doctrine "subscriber". Triggered by the `/create-user` route.
+ *
+ * @see https://github.com/Codeception/module-symfony/issues/34
+ */
+#[AsDoctrineListener(event: Events::prePersist)]
+final class CurrentUserEventListener
+{
+    public ?string $currentUserIdentifier = null;
+
+    public function __construct(private readonly Security $security)
+    {
+    }
+
+    public function prePersist(PrePersistEventArgs $args): void
+    {
+        if (!$args->getObject() instanceof User) {
+            return;
+        }
+
+        $this->currentUserIdentifier = $this->security->getUser()?->getUserIdentifier();
+    }
+}

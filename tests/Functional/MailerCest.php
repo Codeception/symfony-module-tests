61bbad957b549f55fa8cb90f3171f01373404d7a
Aaron Gustavo Nieves <64917965+TavoNiievez@users.noreply.github.com>
2026-06-23 15:58:11 -0500

    Add functional tests for inherited Symfony assertions (#47)
    Covers the assertion/getter methods added in Codeception/module-symfony#240
    that are available on Symfony 6.4:
    
    - DomCrawlerCest: assertSelectorCount, assertAnySelectorTextContains,
      assertAnySelectorTextSame, assertAnySelectorTextNotContains
    - MailerCest: getMailerEvents, getMailerMessages, getMailerMessage
    - MimeCest: assertEmailAddressNotContains, assertEmailSubjectContains,
      assertEmailSubjectNotContains
    
    Browser-history assertions are omitted: they require symfony/browser-kit >= 7.4.
    Refreshes composer.lock.

--- tests/Functional/MailerCest.php
+++ tests/Functional/MailerCest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Functional;
 
 use App\Tests\Support\FunctionalTester;
+use Symfony\Component\Mime\Email;
 
 final class MailerCest
 {
@@ -13,6 +14,26 @@ final class MailerCest
         $I->registerUser('john_doe@gmail.com', '123456', followRedirects: false);
         // There is already an account with this email
         $I->dontSeeEmailIsSent();
+    }
+
+    public function getMailerEvents(FunctionalTester $I)
+    {
+        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
+        $I->assertNotEmpty($I->getMailerEvents());
+    }
+
+    public function getMailerMessage(FunctionalTester $I)
+    {
+        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
+        $I->assertInstanceOf(Email::class, $I->getMailerMessage());
+    }
+
+    public function getMailerMessages(FunctionalTester $I)
+    {
+        $I->registerUser('jane_doe@gmail.com', '123456', followRedirects: false);
+        $messages = $I->getMailerMessages();
+        $I->assertNotEmpty($messages);
+        $I->assertInstanceOf(Email::class, $messages[0]);
     }
 
     public function grabLastSentEmail(FunctionalTester $I)
